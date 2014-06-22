c9b1d068d658448aaa8f9c936bc6526a36ef6f26
Euan <euank@euank.com>
2014-06-22 04:58:19 -0400

    Redo config format, different paths, more general

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'json'
+require 'open3'
 require 'yaml'
 require 'digest/hmac'
 
@@ -7,35 +8,40 @@ require 'digest/hmac'
 config = YAML.load_file("config.yml")
 
 
-post "/webhook" do
-  request.body.rewind
+config.each_pair do |path, info|
+  post path do
+    request.body.rewind
+    body = request.body.read
 
-  body = request.body.read
+    to_run = info["run"]
 
-  if(config && config["secret"])
-    digest = Digest::HMAC.hexdigest(body, config["secret"], Digest::SHA1)
-    if(env["HTTP_X_HUB_SIGNATURE"] != "sha1="+digest)
-      error 403
+    if info["github"]
+      if info["secret"]
+        digest = Digest::HMAC.hexdigest(body, config["secret"], Digest::SHA1)
+
+        error 403 if(env["HTTP_X_HUB_SIGNATURE"] != "sha1="+digest)
+      end
+
+      data = JSON.parse body
+      url = data["repository"]["url"] rescue nil
+      ref = data["ref"] rescue nil
+      short_ref = ref.split('/').last rescue nil
+      short_url = data["repository"]["name"] rescue nil
+
+      unless [url, short_url].include? info["github"]["repo"]
+        error(403, {error: "Wrong repo for this webhook"})
+      end
+      unless [ref, short_ref].include? info["github"]["track"]
+        return {info: "Not tracking this branch, no action taken"}
+      end
     end
+
+    begin
+      stdout, stderr, status = Open3.capture3 to_run
+    rescue Exception => e
+      error(400, {error: "Exceptun running hook script: " + e.to_s})
+    end
+
+    {status: status.exitstatus, stdout: stdout, stderr: stderr}
   end
-
-  data = JSON.parse body
-
-  puts data.to_json
-
-  url = data["repository"]["url"]
-  ref = data["ref"]
-  short_ref = ref.split('/').last
-  short_url = data["repository"]["name"]
-
-  config["repositories"].each{|repo|
-    name = repo.keys.first
-    if([url, short_url].include?(name) &&
-       [ref, short_ref].include?(repo[name]["track"]))
-
-      `#{repo[name]["run"]}`
-    end
-  }
-
-  {ok: 1}
 end
